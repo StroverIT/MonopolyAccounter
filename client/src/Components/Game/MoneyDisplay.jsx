afc1f5cc07cc28d8216f87dd8019b60522360a52
StR0vErBG <xcreaw1@abv.bg>
2022-10-14 03:40:26 -0700

    init of socket client and inner logic

--- client/src/Components/Game/MoneyDisplay.jsx
+++ client/src/Components/Game/MoneyDisplay.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useEffect } from "react";
 import { format } from "../../utils/formatter";
 
-export default function MoneyDisplay({ money, networth }) {
+import { SocketContext } from "../../Routes/socketContext";
+import { useParams } from "react-router-dom";
+
+export default function MoneyDisplay() {
+  const userId = window.location.href.toString().split("#")[1];
+  let { userId: lobbyId } = useParams();
+
+  const { socket } = useContext(SocketContext);
+
+  const [data, setData] = useState({
+    money: 0,
+    networth: 0,
+  });
+
+  useEffect(() => {
+    socket.on("refresh-money-all", (res) => {
+      const user = res.user;
+      if (user) {
+        setData({ money: user.money, networth: user.networth });
+      }
+    });
+    return () => {
+      socket.off("refresh-money-all");
+    };
+  }, []);
+  useEffect(() => {
+    socket.emit(
+      "refresh-money-fn",
+      JSON.stringify({ userId, lobbyId }),
+      (res) => {
+        const data = res.user;
+        setData({ money: data.money, networth: data.networth });
+      }
+    );
+  }, []);
   return (
     <div className="my-5  text-gray-100">
       <div>
-        Money: <span className="pl-1 text-white">{format(money)}</span>
+        Money: <span className="pl-1 text-white">{format(data.money)}</span>
       </div>
       <div>
-        Networth: <span className="pl-1 text-white">{format(networth)}</span>
+        Networth:{" "}
+        <span className="pl-1 text-white">{format(data.networth)}</span>
       </div>
     </div>
   );
