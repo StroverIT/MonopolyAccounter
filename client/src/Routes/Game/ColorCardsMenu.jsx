313e561dde946962803e0a9d205946d9bfeee66d
StR0vErBG <xcreaw1@abv.bg>
2022-10-14 03:41:05 -0700

    chaning to componennt with inner logic

--- client/src/Routes/Game/ColorCardsMenu.jsx
+++ client/src/Routes/Game/ColorCardsMenu.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Button from "../../Components/Button";
+import MoneyDisplay from "../../Components/Game/MoneyDisplay";
 import MoneyStyling from "../../Components/Game/MoneyStyling";
 
 export default function ColorCardsMenu() {
@@ -35,13 +36,7 @@ export default function ColorCardsMenu() {
         <h1 className="text-center">{data.name}</h1>
         <h4 className="text-center">{data.icon}</h4>
         <div className="my-5 text-xs text-gray-100">
-          <div>
-            Money: <span className="pl-1 text-white">{data.money.mil}M</span>{" "}
-            <span className="pl-1 text-white">{data.money.k}k</span>
-          </div>
-          <div>
-            Networth: <span className="text-white">{data.networth}</span>
-          </div>
+          <MoneyDisplay />
         </div>
         <h1 className="text-center">Card Menu</h1>
         <div className="flex items-center justify-center mt-5">
