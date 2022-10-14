aef798a5bcf3e9067a998d8dc9cc6f744fab30f4
StR0vErBG <xcreaw1@abv.bg>
2022-10-14 03:41:40 -0700

    init of socket

--- client/src/Routes/Lobby/LobbyProfile.jsx
+++ client/src/Routes/Lobby/LobbyProfile.jsx
@@ -1,15 +1,82 @@
-import React, { useState } from "react";
-import { Link, Route } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import React, { useContext, useState } from "react";
+
+// Components
+import Input from "../../Components/Input";
+
+// Icons
+import { GrReturn } from "react-icons/gr";
+import { Link, useParams } from "react-router-dom";
 import Button from "../../Components/Button";
 
-export default function LobbyProfile() {
-  const { id } = useParams();
+// Context
+import { SocketContext } from "../socketContext";
+// Navigatin
+import { useNavigate } from "react-router-dom";
 
-  // From here use hashesesh to navigate through the game !
+export default function CreateLobby() {
+  const { lobbyId } = useParams();
+  let navigate = useNavigate();
+
+  const { socket } = useContext(SocketContext);
+
+  const [inputsForm, setInputsForm] = useState({
+    fullName: "",
+    icon: "",
+  });
+  const inputHandler = (e) => {
+    setInputsForm((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const submitHandler = async (e) => {
+    socket.emit(
+      "create-user",
+      JSON.stringify({
+        fullName: inputsForm.fullName,
+        icon: inputsForm.icon,
+        lobbyId,
+        socketId: socket.id,
+      }),
+      (response) => {
+        navigate(`/lobby/${lobbyId}#${response.user._id}`);
+      }
+    );
+  };
   return (
-    <div className="container text-gray">
-      <div className="px-2 text-white">Main</div>
+    <div className="flex items-center justify-center h-screen ">
+      <div className="w-10/12 bg-gray h-5/6 ">
+        <div className="flex items-center justify-center px-4 pt-5 pb-5">
+          <Link to="/lobby" className="mr-auto text-primary-100">
+            <GrReturn />
+          </Link>
+          <h1 className="mr-auto text-xl text-center text-primary-100">
+            Your profile
+          </h1>
+        </div>
+        <div className="flex flex-col px-4 space-y-5">
+          <div className="">
+            <Input
+              onChange={inputHandler}
+              name="fullName"
+              placeholder="Full name"
+              value={inputsForm.fullName}
+            />
+          </div>
+          <div className="">
+            <Input
+              onChange={inputHandler}
+              name="icon"
+              placeholder="Icon / figure"
+              value={inputsForm.icon}
+            />
+          </div>
+
+          <Button onClick={submitHandler} theme="primary200-white">
+            Create profile
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
