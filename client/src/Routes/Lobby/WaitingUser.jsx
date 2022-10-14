8d24e965db50e8687fb3621006af9aaabeebbb63
StR0vErBG <xcreaw1@abv.bg>
2022-10-14 03:41:47 -0700

    init of socket

--- client/src/Routes/Lobby/WaitingUser.jsx
+++ client/src/Routes/Lobby/WaitingUser.jsx
@@ -1,22 +1,55 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GrReturn } from "react-icons/gr";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+// Components
+import Button from "../../Components/Button";
+// Context
+import { SocketContext } from "../socketContext";
 
 export default function WaitingUser() {
-  const [users, setUsers] = useState([
-    { name: "Emil Zlatinov", role: "lobbyCreator", icon: "Шапка", _id: 212312 },
-    {
-      name: "Savi Bojilova",
-      role: "player",
-      icon: "Китайски Дракон",
-      _id: 51251,
-    },
-    { name: "My Doggy", role: "player", icon: "Кенгуру", _id: 5363121 },
-    { name: "Pepi Petrov", role: "player", icon: "Статуя", _id: 21516126 },
-    { name: "Kirilica Petrova", role: "player", icon: "Кола", _id: 713 },
-    { name: "My aunt", role: "player", icon: "Сумо борец", _id: 2 },
-  ]);
+  let { id } = useParams();
+  let navigate = useNavigate();
 
+  const { socket, lobbies, setLobbies } = useContext(SocketContext);
+
+  const [users, setUsers] = useState([]);
+  const [currUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const userId = window.location.href.toString().split("#")[1];
+
+    socket.emit(
+      "get-lobby-users",
+      JSON.stringify({ lobbyId: id }),
+      (response) => {
+        const joined = response.lobby.joinedPlayers;
+        setUsers(joined);
+        const foundCreator = joined.find((user) => user._id == userId);
+        setCurrentUser(foundCreator);
+      }
+    );
+    socket.on("receive-user", (data) => {
+      console.log("data", data);
+      setUsers((prevState) => [...prevState, data.user]);
+    });
+    socket.on("change-to-menu", (response) => {
+      const pageRed = `/game/${id}#${userId}`;
+
+      navigate(pageRed);
+    });
+    return () => {
+      socket.off("receive-user");
+      socket.off("change-to-menu");
+    };
+  }, []);
+
+  const startGameHandler = () => {
+    const data = JSON.stringify({ lobby: id });
+    socket.emit("start-game", data, (response) => {
+      const pageRed = `${response.page}#${currUser._id}`;
+      console.log(pageRed);
+      navigate(pageRed);
+    });
+  };
   return (
     <div className="flex items-center justify-center h-screen ">
       <div className="w-10/12 bg-gray h-5/6 ">
@@ -29,11 +62,11 @@ export default function WaitingUser() {
               Waiting for players...
             </h1>
           </div>
-          <div className="  text-xl text-gray-100 text-center ">
+          <div className="text-xl text-center text-gray-100 ">
             {users.length}/6
           </div>
           <div className=" mt-28">
-            <div className="px-2 mt-5 grid grid-cols-2 gap-y-4 ">
+            <div className="grid grid-cols-2 px-2 mt-5 gap-y-4 ">
               {users.map((user) => {
                 return (
                   <div className="text-center" key={user._id}>
@@ -44,15 +77,22 @@ export default function WaitingUser() {
                           : "text-primary"
                       }`}
                     >
-                      {user.name}
+                      {user.fullName}
                     </div>
-                    <div className="text-xs  text-primary-200 text-center">
+                    <div className="text-xs text-center text-primary-200">
                       {user.icon}
                     </div>
                   </div>
                 );
               })}
             </div>
+            {currUser?.role == "lobbyCreator" && (
+              <div className="flex items-center justify-center mt-56 ">
+                <Button theme="primary200-white" onClick={startGameHandler}>
+                  Start game
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
