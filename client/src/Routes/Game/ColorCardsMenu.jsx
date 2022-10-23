9e88084ef872028b8f5444575eb5411ed93182ee
StR0vErBG <xcreaw1@abv.bg>
2022-10-23 18:43:56 +0300

    Importing them as component

--- client/src/Routes/Game/ColorCardsMenu.jsx
+++ client/src/Routes/Game/ColorCardsMenu.jsx
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { GiHamburgerMenu, GiHouseKeys } from "react-icons/gi";
 import Button from "../../Components/Button";
+import PowerPlant from "../../Components/Game/Cards/PowerPlant";
+import PropertyCard from "../../Components/Game/Cards/Property";
+import TransportCard from "../../Components/Game/Cards/Transport";
 import MoneyDisplay from "../../Components/Game/MoneyDisplay";
 import MoneyStyling from "../../Components/Game/MoneyStyling";
 import { format } from "../../utils/formatter";
@@ -89,6 +92,17 @@ export default function ColorCardsMenu() {
     console.log(isMortgage);
     setColorOnMortgage(isMortgage);
   }, [cards]);
+  const cardFunctionalities = {
+    isBought: true,
+    isBuildings: isBuildings,
+    mortgageHandler: mortgageHandler,
+    calcTotal: calcTotal,
+    isColorOnMortgage: isColorOnMortgage,
+    cardShow: cardShow,
+    highestNumHouse: highestNumHouse,
+    upgradeHouse: upgradeHouse,
+    downgradeHouse: downgradeHouse,
+  };
   return (
     <div className="flex items-center justify-center h-full min-h-screen">
       <div className="relative w-10/12 bg-gray h-5/6 my-10">
@@ -113,122 +127,17 @@ export default function ColorCardsMenu() {
         <div className="grid lg:grid-cols-3 lg:space-x-5 space-y-5 px-5 my-10">
           {cards?.properties?.map((card) => {
             return (
-              <div className="bg-gray-100 " key={card._id}>
-                <div
-                  className={`w-full ${card.colorToDisplay} flex items-center justify-between  py-4 text-xl font-bold px-2`}
-                >
-                  <div className="">
-                    <div>{card?.name}</div>
-                    <div className="text-sm text-center text-primary">
-                      {card.housesBought}
-                    </div>
-                  </div>
-                  {!card?.isOnMortgage && isBuildings <= 0 && (
-                    <div className="relative text-[0.9rem] text-xs ">
-                      <button
-                        className="py-1 bg-gray text-white
-                       rounded-full px-4 font-thin"
-                        onClick={() => mortgageHandler(card._id, "mortgage")}
-                      >
-                        Ипотекирай
-                      </button>
-                      <div className="text-center text-green mt-1">
-                        +{format(card.mortgagePrice)}
-                      </div>
-                    </div>
-                  )}
-                  {card.isOnMortgage && isBuildings <= 0 && (
-                    <div className="relative text-[0.9rem] text-xs ">
-                      <button
-                        className="py-1 bg-gray text-white
-                       rounded-full px-4 font-thin"
-                        onClick={() =>
-                          mortgageHandler(card._id, "returnMortgage")
-                        }
-                      >
-                        Върни имота
-                      </button>
-                      <div className="text-center text-red mt-1">
-                        -{format(card.mortgagePrice * 1.1)}
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <h3 className="my-2 text-center">
-                  Наем <span className="pl-1">{format(card.price)}</span>
-                </h3>
-                <div className="px-4 pb-5">
-                  <div className="flex justify-between">
-                    <h3>С 1 къща</h3>
-                    <div className="font-semibold">{format(card.oneHouse)}</div>
-                  </div>
-                  <div className="flex justify-between">
-                    <h3>С 2 къщи</h3>
-                    <div className="font-semibold">
-                      {format(card.twoHouses)}
-                    </div>
-                  </div>
-                  <div className="flex justify-between">
-                    <h3>С 3 къщи</h3>
-                    <div className="font-semibold">
-                      {format(card.threeHouses)}
-                    </div>
-                  </div>
-                  <div className="flex justify-between">
-                    <h3>С 4 къщи</h3>
-                    <div className="font-semibold">
-                      {format(card.fourHouses)}
-                    </div>
-                  </div>
-                  <div className="flex justify-between">
-                    <h3>С ХОТЕЛ</h3>
-                    <div className="font-semibold">{format(card.hotel)}</div>
-                  </div>
-                  <h3 className="mt-2 mb-2 font-semibold text-center">
-                    Ипотечна стойност {format(card.mortgagePrice)}
-                  </h3>
-                  <h3 className="mb-1 text-xs text-center">
-                    Къщите струват по {format(card.housePrice)} всяка.
-                  </h3>
-                  <h3 className="text-xs text-center">
-                    Хотелите струват по {format(card.housePrice)}, всеки плюс 4
-                    къщи
-                  </h3>
-                  {calcTotal === "All" && !isColorOnMortgage && (
-                    <div className="flex text-center justify-between mt-10 flex-col  text-xs space-y-5">
-                      {isBuildings < cardShow?.total * 5 &&
-                        card.housesBought < highestNumHouse && (
-                          <div>
-                            <Button
-                              theme="primary200-white"
-                              onClick={() =>
-                                upgradeHouse(card._id, card.housesBought)
-                              }
-                            >
-                              Upgrade
-                            </Button>
-                            <div className="text-red">
-                              -{format(cardShow?.upgrade)}
-                            </div>
-                          </div>
-                        )}
-                      {card.housesBought > 0 && (
-                        <div>
-                          <Button
-                            theme="primary200-white"
-                            onClick={() => downgradeHouse(card._id)}
-                          >
-                            Downgrade
-                          </Button>
-                          <div className="text-green">
-                            +{format(cardShow?.downgrade)}
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  )}
-                </div>
-              </div>
+              <>
+                {card.type === "property" && (
+                  <PropertyCard card={card} boughtData={cardFunctionalities} />
+                )}
+                {card.type === "transport" && (
+                  <TransportCard card={card} boughtData={cardFunctionalities} />
+                )}
+                {card.type === "power_plant" && (
+                  <PowerPlant card={card} boughtData={cardFunctionalities} />
+                )}
+              </>
             );
           })}
         </div>
