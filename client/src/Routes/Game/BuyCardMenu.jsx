53db0910ec5eebc941634c361d31ab3e2761e1df
StR0vErBG <xcreaw1@abv.bg>
2022-10-23 18:43:51 +0300

    Importing them as component

--- client/src/Routes/Game/BuyCardMenu.jsx
+++ client/src/Routes/Game/BuyCardMenu.jsx
@@ -8,6 +8,9 @@ import MoneyDisplay from "../../Components/Game/MoneyDisplay";
 import { GameRouterContext } from "./GameRouterContext";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../socketContext";
+import PropertyCard from "../../Components/Game/Cards/Property";
+import TransportCard from "../../Components/Game/Cards/Transport";
+import PowerPlant from "../../Components/Game/Cards/PowerPlant";
 
 const user = {
   fullName: "Emil Z.",
@@ -55,47 +58,10 @@ export default function BuyCardMenu({ data }) {
           <h5 className="text-sm text-white">{user.icon}</h5>
         </div>
         <MoneyDisplay />
-        <div className="bg-gray-100 ">
-          <div
-            className={`w-full ${card.colorToDisplay} text-center py-4 text-xl font-bold`}
-          >
-            {card.name}
-          </div>
-          <h3 className="my-2 text-center">
-            Наем <span className="pl-1">{format(card.price)}</span>
-          </h3>
-          <div className="px-4 pb-5">
-            <div className="flex justify-between">
-              <h3>С 1 къща</h3>
-              <div className="font-semibold">{format(card.oneHouse)}</div>
-            </div>
-            <div className="flex justify-between">
-              <h3>С 2 къщи</h3>
-              <div className="font-semibold">{format(card.twoHouses)}</div>
-            </div>
-            <div className="flex justify-between">
-              <h3>С 3 къщи</h3>
-              <div className="font-semibold">{format(card.threeHouses)}</div>
-            </div>
-            <div className="flex justify-between">
-              <h3>С 4 къщи</h3>
-              <div className="font-semibold">{format(card.fourHouses)}</div>
-            </div>
-            <div className="flex justify-between">
-              <h3>С ХОТЕЛ</h3>
-              <div className="font-semibold">{format(card.hotel)}</div>
-            </div>
-            <h3 className="mt-2 mb-2 font-semibold text-center">
-              Ипотечна стойност {format(card.mortgagePrice)}
-            </h3>
-            <h3 className="mb-1 text-xs text-center">
-              Къщите струват по {format(card.housePrice)} всяка.
-            </h3>
-            <h3 className="text-xs text-center">
-              Хотелите струват по {format(card.housePrice)}, всеки плюс 4 къщи
-            </h3>
-          </div>
-        </div>
+        {card.type === "property" && <PropertyCard card={card} />}
+        {card.type === "transport" && <TransportCard card={card} />}
+        {card.type === "power_plant" && <PowerPlant card={card} />}
+
         <section className="flex flex-col mt-10 space-y-5">
           <div className="flex flex-col items-center justify-center">
             {/* If not enough money only show auction menu */}
