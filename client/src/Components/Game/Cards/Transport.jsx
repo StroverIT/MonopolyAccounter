86a565c2c0ab03763488a9b1e964cf907d3a1849
StR0vErBG <xcreaw1@abv.bg>
2022-10-23 18:43:39 +0300

    Property component with full functionalities

---
+++ client/src/Components/Game/Cards/Transport.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { format } from "../../../utils/formatter";
+
+export default function TransportCard({ card, boughtData }) {
+  return (
+    <div className="bg-gray-100 ">
+      <div
+        className={`w-full ${card.colorToDisplay} text-center py-4 text-xl font-bold`}
+      >
+        {card.name}
+      </div>
+      {!card.isOnMortgage && boughtData?.isBought && (
+        <div className="relative text-[0.9rem] text-xs flex items-center justify-center flex-col">
+          <button
+            className="py-1 bg-gray text-white
+                       rounded-full px-4 font-thin"
+            onClick={() => boughtData.mortgageHandler(card._id, "mortgage")}
+          >
+            Ипотекирай
+          </button>
+          <div className="text-center text-green mt-1">
+            +{format(card.mortgagePrice)}
+          </div>
+        </div>
+      )}
+      {card.isOnMortgage && boughtData?.isBought && (
+        <div className="relative text-[0.9rem] text-xs flex items-center justify-center flex-col">
+          <button
+            className="py-1 bg-gray text-white
+                       rounded-full px-4 font-thin"
+            onClick={() =>
+              boughtData.mortgageHandler(card._id, "returnMortgage")
+            }
+          >
+            Върни имота
+          </button>
+          <div className="text-center text-red mt-1">
+            -{format(card.mortgagePrice * 1.1)}
+          </div>
+        </div>
+      )}
+      <h3 className="my-2 text-center">
+        Наем <span className="pl-1">{format(card.price)}</span>
+      </h3>
+      <div className="px-4 pb-5">
+        <div className="flex justify-between">
+          <h3>Ако притежавате 2 транспортни компании</h3>
+          <div className="font-semibold">{format(card.twoTransport)}</div>
+        </div>
+        <div className="flex justify-between">
+          <h3>Ако притежавате 3 транспортни компании</h3>
+          <div className="font-semibold">{format(card.threeTransport)}</div>
+        </div>
+        <div className="flex justify-between">
+          <h3>Ако притежавате 4 транспортни компании</h3>
+          <div className="font-semibold">{format(card.fourTransport)}</div>
+        </div>
+
+        <h3 className="mt-2 mb-2 font-semibold text-center">
+          Ипотечна стойност {format(card.mortgagePrice)}
+        </h3>
+      </div>
+    </div>
+  );
+}
